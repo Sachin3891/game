f92b95a78f68568c1ea30d21837bb870b2569fb1
Sachin3891 <sachinraj3891@gmail.com>
2023-08-01 09:28:18 +0530

    3rd commit

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const start_btn = document.querySelector(".start_btn button");
 const info_box = document.querySelector(".info_box");
+
 const continue_btn = info_box.querySelector(".buttons .restart");
 const quiz_box = document.querySelector(".quiz_box");
 const result_box = document.querySelector(".result_box");
@@ -39,7 +40,7 @@ restart_quiz.onclick = ()=>{
     widthValue = 0;
     showQuetions(que_count); 
     queCounter(que_numb); 
-    clearInterval(counter);
+    clearInterval(counter); 
     clearInterval(counterLine); 
     startTimer(timeValue); 
     startTimerLine(widthValue); 
@@ -52,7 +53,7 @@ const bottom_ques_counter = document.querySelector("footer .total_que");
 
 next_btn.onclick = ()=>{
     if(que_count < questions.length - 1){ 
-        que_count++; 
+        que_count++;
         que_numb++; 
         showQuetions(que_count); 
         queCounter(que_numb); 
@@ -63,9 +64,9 @@ next_btn.onclick = ()=>{
         timeText.textContent = "Time Left"; 
         next_btn.classList.remove("show"); 
     }else{
-        clearInterval(counter);
-        clearInterval(counterLine);
-        showResult();
+        clearInterval(counter); 
+        clearInterval(counterLine); 
+        showResult(); 
     }
 }
 
@@ -81,12 +82,10 @@ function showQuetions(index){
     option_list.innerHTML = option_tag; 
     
     const option = option_list.querySelectorAll(".option");
-   
+    
     for(i=0; i < option.length; i++){
         option[i].setAttribute("onclick", "optionSelected(this)");
     }
-
-    
 
 }
 
@@ -94,7 +93,7 @@ let tickIconTag = '<div class="icon tick"><i class="fas fa-check"></i></div>';
 let crossIconTag = '<div class="icon cross"><i class="fas fa-times"></i></div>';
 
 function optionSelected(answer){
-    clearInterval(counter); 
+    clearInterval(counter);
     clearInterval(counterLine); 
     let userAns = answer.textContent; 
     let correcAns = questions[que_count].answer; 
@@ -102,8 +101,8 @@ function optionSelected(answer){
     
     if(userAns == correcAns){ 
         userScore += 1; 
-        answer.classList.add("correct");
-        answer.insertAdjacentHTML("beforeend", tickIconTag);on
+        answer.classList.add("correct"); 
+        answer.insertAdjacentHTML("beforeend", tickIconTag); 
         console.log("Correct Answer");
         console.log("Your correct answers = " + userScore);
     }else{
@@ -149,13 +148,13 @@ function startTimer(time){
         time--; 
         if(time < 9){ 
             let addZero = timeCount.textContent; 
-            timeCount.textContent = "0" + addZero;
+            timeCount.textContent = "0" + addZero; 
         }
         if(time < 0){ 
-            clearInterval(counter);
+            clearInterval(counter); 
             timeText.textContent = "Time Off"; 
-            const allOptions = option_list.children.length;
-            let correcAns = questions[que_count].answer;
+            const allOptions = option_list.children.length; 
+            let correcAns = questions[que_count].answer; 
             for(i=0; i < allOptions; i++){
                 if(option_list.children[i].textContent == correcAns){ 
                     option_list.children[i].setAttribute("class", "option correct"); 
@@ -181,7 +180,7 @@ function startTimerLine(time){
     }
 }
 function queCounter(index){
-    
+   
     let totalQueCounTag = '<span><p>'+ index +'</p> of <p>'+ questions.length +'</p> Questions</span>';
-    bottom_ques_counter.innerHTML = totalQueCounTag;  
+    bottom_ques_counter.innerHTML = totalQueCounTag; 
 }
